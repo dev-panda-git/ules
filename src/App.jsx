89e04f7c55cd94f681ad99bd2e9f32d8e61f935d
dev_panda <tohirbabs@gmail.com>
2024-05-24 15:07:43 +0100

    hero design

--- src/App.jsx
+++ src/App.jsx
@@ -2,33 +2,59 @@ import "./App.css";
 
 function App() {
   return (
-    <div className="h-screen w-screen bg-black text-white p-5 font-mono">
-      <header className="absolute top-0 flex items-center justify-between">
-        <h1>ules</h1>
+    <div className="h-screen w-screen bg-black text-white font-mono">
+      <header className="absolute top-0 flex items-center justify-between p-8 w-full">
+        <h1>
+          <img
+            src="/logo.jpg"
+            alt=""
+            className="w-10 h-10 inline-block align-middle"
+          />{" "}
+          ules
+        </h1>
         <div className=""></div>
-        <nav></nav>
+        <nav className="flex gap-3">
+          <a href="" className="px-6 p-3 rounded-full bg-white text-black">
+            about
+          </a>
+
+          <a href="" className="px-6 p-3 rounded-full bg-white text-black">
+            sponsorship
+          </a>
+          <a href="" className="px-6 p-3 rounded-full bg-white text-black">
+            blog
+          </a>
+          <a href="" className="px-6 p-3 rounded-full bg-white text-black">
+            contact
+          </a>
+          <a href="" className="px-6 p-3 rounded-full bg-blue-500 text-white">
+            join us
+          </a>
+        </nav>
       </header>
-      <div className="flex h-full items-center">
+      <div className="flex h-full items-center  p-4">
         <div className="flex-1 flex flex-col gap-3">
           <p></p>
-          <p className="text-7xl capitalize font-bold">
-            ules is a great society so you should probably join us
+          <p className="text-8xl capitalize">
+            a student <span className="text-blue-500">society</span> of aspiring
+            engineers
           </p>
-          <div className="flex gap-2">
-            <button className="rounded-full border px-4 p-2">button</button>
-            <button className="rounded-full border px-4 p-2">
+          <div className="flex gap-3">
+            <button className="rounded-full border px-6 p-3">button</button>
+            <button className="rounded-full border px-6 p-3">
               another button
             </button>
           </div>
         </div>
         <div className="flex-1 h-full">
           <img
-            src="/square.jpg"
+            src="/bench.jpg"
             alt=""
             className="rounded-3xl object-cover h-full"
           />
         </div>
       </div>
+      <div className=""></div>
     </div>
   );
 }
